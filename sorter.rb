3801e8cbc33dfdcdad29f80ff6f240276d193f0f
Serheo Shatunov <sshatunov@yandex.ru>
2012-03-25 21:45:27 +0400

    output & format fix

--- sorter.rb
+++ sorter.rb
@@ -4,12 +4,10 @@
 # Other files and format will be ingored
 # Shatunov S. MIT License. 
 # ruby 1.9.2 tested
-require 'pp'
-
 
 list = Dir[File.join("./", "*.strings")]
-puts "Files: " + list.to_s
-
+puts "Files found: " + list.to_s 
+puts
 
 list.each do |file|
   buffer = Array.new
@@ -17,13 +15,14 @@ list.each do |file|
   
   File.open(file, 'r') do |f1|  
     while line = f1.gets  
-      if (line =~ /^\"[a-zA-Z0-9]+\"\s{,2}=\s{,2}\".+\";$/)
+      if (line =~ /^\"[a-zA-Z0-9]+\"\s{,2}=\s{,2}\".+\";/)
         buffer.push line
       else
         unsort.push line
       end 
     end  
   end
+  
   buffer = buffer.sort
   
   File.open(file, 'w') do |f1|  
@@ -31,8 +30,10 @@ list.each do |file|
     buffer.each {|line|  f1.write(line)}
   end
   
-  puts (unsort.count + buffer.count).to_s +  " lines rewrited in " + file
-  
+  puts "File " + file
+  puts "unsorted:" + unsort.to_s
+  puts "Rewrited lines count: " + (unsort.count + buffer.count).to_s
+  puts
 end
 
 puts "Complete!"
